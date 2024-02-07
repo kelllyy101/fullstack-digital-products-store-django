310bd5779ba00609ae538bcd03483657474b3e9a
kelllyy101 <kellyom81@hotmail.com>
2024-02-07 16:25:27 +0000

    authall templates

--- best_me/settings.py
+++ best_me/settings.py
@@ -59,7 +59,8 @@ ROOT_URLCONF = 'best_me.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
