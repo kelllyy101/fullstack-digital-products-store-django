40e11519c8bae1999f61310f5f1443377e57afea
kelllyy101 <kellyom81@hotmail.com>
2024-02-19 09:11:58 +0000

    added context for shopping bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,14 +1,27 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
-def bag_contents(total):
+def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
-
+    bag = request.session.get('bag', {})
     discount = Decimal(0)
-    # Apply discount if total exceeds €20.00
+    
 
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+    # Apply discount if total exceeds €20.00
     if total > Decimal('20.00'):
         discount = total * Decimal('0.10')  # 10% discount
     
--- bag/views.py
+++ bag/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render, redirect
 
-# Create your views here.
-from django.shortcuts import render
-
-# Create your views here.
-
 def view_bag(request):
     """ A view that renders the bag contents page """
 
@@ -12,7 +7,7 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
+    
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
