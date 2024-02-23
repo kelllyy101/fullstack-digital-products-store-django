74c36888818336d0776c0384a73a25bc1b67d450
kelllyy101 <kellyom81@hotmail.com>
2024-02-23 19:08:58 +0000

    update quantity bag function added and remove items

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 def view_bag(request):
     """ A view that renders the bag contents page """
@@ -20,3 +20,57 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     print(request.session['bag'])
     return redirect(redirect_url)
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag, try to catch any exceptions"""
+
+    try:
+        bag = request.session.get('bag', {})
+        
+        if item_id in bag:  # Check if the item exists in the bag
+            del bag[item_id]  # Remove the item from the bag
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
+
+
+
+# def remove_from_bag(request, item_id):
+#     """Remove the item from the shopping bag"""
+
+#     try:
+#         size = None
+#         if 'product_size' in request.POST:
+#             size = request.POST['product_size']
+#         bag = request.session.get('bag', {})
+
+#         if size:
+#             del bag[item_id]['items_by_size'][size]
+#             if not bag[item_id]['items_by_size']:
+#                 bag.pop(item_id)
+#         else:
+#             bag.pop(item_id)
+
+#         request.session['bag'] = bag
+#         return HttpResponse(status=200)
+
+#     except Exception as e:
+#         return HttpResponse(status=500)
