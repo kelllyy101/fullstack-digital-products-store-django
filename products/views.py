ccabe7db7c72b1b16197bad228503ea5ea0a280c
kelllyy101 <kellyom81@hotmail.com>
2024-03-03 14:31:33 +0000

    added product view undersprofile management

--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product, Category
 from django.db.models.functions import Lower
-from .forms import ProuductForm
+from .models import Product, Category
+
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -70,7 +71,17 @@ def product_description(request, product_id):
 
 def add_product(request):
     """ Add a product to the store """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
     template = 'products/add_product.html'
     context = {
         'form': form,
