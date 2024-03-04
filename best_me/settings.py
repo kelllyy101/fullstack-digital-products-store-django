775ae427d3c00b76c31e9efde1c5ea059df6d30a
kelllyy101 <kellyom81@hotmail.com>
2024-03-04 08:47:01 +0000

    added Profile, allowed hosts and database for deployment

--- best_me/settings.py
+++ best_me/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-n$-b^+^xb-39ea4_$*6@_oz=agp=6tv55ur9xefm2sgaii59ji
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://the-best-me-38fd42c32230.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -119,12 +121,15 @@ WSGI_APPLICATION = 'best_me.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
+
+   
 
 
 # Password validation
