ba9d93e366aeab8915ca9401f74a362f1755bf47
kelllyy101 <kellyom81@hotmail.com>
2024-03-11 17:13:09 +0000

    add blog forms

---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Post
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'author', 'body')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}), #from Bootstrap instead of static files
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView #LV queryset for us in the database that we can list, detail brings the detail of one record
 from .models import Post
+from .forms import BlogPostForm
+
 # Create your views here.
 #def view_blog(request):
 #    return render(request, 'blog.html')
