491c192fb9ba56d1983d0db6f882998ff663365f
kelllyy101 <kellyom81@hotmail.com>
2024-03-15 11:15:46 +0000

    added base app testing for best_me

---
+++ best_me/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class BaseAppTests(TestCase):
+    def test_admin_url(self):
+        response = self.client.get(reverse('admin:index'))
+        self.assertEqual(response.status_code, 302)  # Ensure redirection to login page
+
+    def test_accounts_url(self):
+        response = self.client.get(reverse('account_login'))
+        self.assertEqual(response.status_code, 200)  # Ensure login page is accessible
+
+    def test_home_url(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)  # Ensure home page is accessible
+
+    # Repeat the above pattern for other URLs in urlpatterns
+
+    # You can also write tests for static and media files serving, if applicable
