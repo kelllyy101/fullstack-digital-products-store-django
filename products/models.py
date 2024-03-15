4e2662ee1bb28afe154d89de22451ccc938a2275
kelllyy101 <kellyom81@hotmail.com>
2024-03-15 22:59:30 +0000

    Use product images from s3

--- products/models.py
+++ products/models.py
@@ -24,8 +24,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.URLField()
     url = models.URLField()
 
     def __str__(self):
