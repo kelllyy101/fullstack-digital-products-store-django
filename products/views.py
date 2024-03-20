233117823139d2b935294b51bdc145460215cc50
kelllyy101 <kellyom81@hotmail.com>
2024-03-20 21:04:12 +0000

    add search query to products view

--- products/views.py
+++ products/views.py
@@ -38,6 +38,14 @@ def all_products(request):
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
             
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
 
