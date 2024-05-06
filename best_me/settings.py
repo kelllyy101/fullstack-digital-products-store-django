e72af5e8230d987d3ebaa1724541d280e29b6926
kelllyy101 <kellyom81@hotmail.com>
2024-05-06 16:22:16 +0100

    added target=_blank so social media links open on separate page

--- best_me/settings.py
+++ best_me/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n$-b^+^xb-39ea4_$*6@_oz=agp=6tv55ur9xefm2sgaii59ji'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
