2374947ace48f0bca35f29c82432f8051ecc3d7f
kelllyy101 <kellyom81@hotmail.com>
2024-05-20 22:29:56 +0100

    added image url field for adding new blog posts

--- blog/forms.py
+++ blog/forms.py
@@ -4,13 +4,14 @@ from .models import Post, BlogCategory, Comment
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'category', 'body', 'blog_snippet')
+        fields = ('title', 'title_tag', 'author', 'category', 'body', 'blog_snippet', 'image')
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}), #from Bootstrap instead of static files
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             'blog_snippet': forms.Textarea(attrs={'class': 'form-control'}),
+            'image:': forms.URLField(),
         }
 
 class EditForm(forms.ModelForm): #form to edit blog
